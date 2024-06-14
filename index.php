3b3872e5b6b15bcc4528ab1b26244c89f9fe801d
i miss u <153477665+E0x00000@users.noreply.github.com>
2024-06-14 11:19:52 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Define o cabeçalho Content-Type para texto simples
+header("Content-Type: text/plain");
+
+// Obtém a data atual e formata no formato desejado
+$dataAtual = date("d/m/Y");
+
+// Retorna a data atual formatada
+echo $dataAtual;
+?>
